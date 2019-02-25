ad3688483782696b8c0e9fd189bbb8900c9d62bf
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-25 12:39:34 -0800

    Make Grub in ES6 using constructor() {}  or eat() {}

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
 class Grub {
+    constructor() {
+        this.age = 0;
+        this.color = 'pink';
+        this.food = 'jelly'    
+    }
+    eat() {
+        return 'Mmmmmmmmm jelly'; 
+    }
 };
+
+
+//read: https://teamtreehouse.com/community/does-arrow-function-syntax-works-for-prototype
+
