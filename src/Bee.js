56092ebdc89fa0ecffd76f0ca8edfd9540a7b9b0
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-25 12:41:59 -0800

    Attempted Bee extends Grub
    Bee Class Functionality error: "before each" hook for "should have an `age` property that is set to `5`" ‣
    ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
        at new Bee (file:///Users/vickie/Documents/GitHub/hrsf113-6ees6ees6ees/src/Bee.js:3:5)
        at Context.beforeEach (Bee.js:4:26)

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor() {
+    this.age = 5;
+    this.color = 'yellow';
+    //food property inherit from grub
+    this.job = 'keep on growing';
+  }
+
+  //eat(){} method inherit from grub
+
 };
