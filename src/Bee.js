39c6ce4a69a9fe0794ff7f7fc2880e0c955aab95
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-03-04 11:22:38 -0800

    Implemented Bee with extends constructor()  and super()

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,10 @@
 class Bee extends Grub {
   constructor() {
+    super();
     this.age = 5;
     this.color = 'yellow';
     this.job = 'keep on growing';
   }
 };
+
+//read: https://medium.com/beginners-guide-to-mobile-web-development/super-and-extends-in-javascript-es6-understanding-the-tough-parts-6120372d3420
