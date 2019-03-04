1fb2b2a79ad49c1dea228b0f9782ec7295ea86c8
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-03-04 11:21:26 -0800

    Attempt extending Bee

--- src/Bee.js
+++ src/Bee.js
@@ -2,9 +2,6 @@ class Bee extends Grub {
   constructor() {
     this.age = 5;
     this.color = 'yellow';
-    //food property inherit from grub
     this.job = 'keep on growing';
   }
-
-  //eat(){} method inherit from grub
 };
